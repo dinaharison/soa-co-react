f86e18e5a2ae3814cdeac90db3ac6f3d943a9808
dinaharison <dinaharison.jb@gmail.com>
2024-11-20 11:37:36 +0300

    changed page title

--- frontend/src/authentication/hooks/useAuthStore.test.js
+++ frontend/src/authentication/hooks/useAuthStore.test.js
@@ -8,6 +8,8 @@ describe("useAuthStore", () => {
     result.current.loginUser(true, "token", "username");
 
     expect(result.current.user.token).toBe("token");
+    expect(result.current.user.isLoggedIn).toBe(true);
+    expect(result.current.user.username).toBe("username");
   });
 
   it("test if useAuthStore deletes user credentials correctly", () => {
@@ -15,5 +17,7 @@ describe("useAuthStore", () => {
     result.current.logoutUser();
 
     expect(result.current.user.token).toBe(null);
+    expect(result.current.user.isLoggedIn).toBe(false);
+    expect(result.current.user.username).toBe(null);
   });
 });
